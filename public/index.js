c00309e807e753b774db3928b33c14ed37d4e117
nikitosych <boriszyn@gmail.com>
2024-08-31 21:38:16 +0200

    main func is now async

--- public/index.js
+++ public/index.js
@@ -3,11 +3,13 @@
 
 const serverURL = "http://localhost:8000/";
 
-function main() {
-    fetch(new URL("/api/data", serverURL))
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-        .catch((e) => console.error("Clientside fetch error occured:", e));
+async function main() {
+    const response = await fetch(new URL("/api/data?timestamp=" + Date.now(), serverURL)) // timestamp нужен для предотвращения загрузки кешированных данных браузером
+    const body = await response.json()
+
+    console.log(body);
+
+
     fetch(new URL("/api/webhook", serverURL))
         .then((res) => res.json())
         .then((data) => console.log(data))
